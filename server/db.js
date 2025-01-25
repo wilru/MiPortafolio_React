b31be6e1edca856d8de3e0e0d49db6d30d00e8c3
wilru25 <wilru25@hotmail.com>
2025-01-25 16:57:28 -0500

    Update package.json, remove server ajuste db.js

--- server/db.js
+++ server/db.js
@@ -1,21 +1,11 @@
 const Pool = require('pg').Pool;
 require('dotenv').config();
 
-/*const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME
-});
-
-module.exports = pool;
-*/
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
-      rejectUnauthorized: false
+        rejectUnauthorized: false
     }
-  });
-  
-  module.exports = pool;
+});
+
+module.exports = pool;
